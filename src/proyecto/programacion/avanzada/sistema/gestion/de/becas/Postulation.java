e947294394b4055272d16ea1f6786347ca6c940f
rpaez <rpaez@10.107.247.178>
2025-08-25 13:27:36 -0400

    Creación de Clase Postulacion

---
+++ src/proyecto/programacion/avanzada/sistema/gestion/de/becas/Postulation.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyecto.programacion.avanzada.sistema.gestion.de.becas;
+
+/**
+ *
+ * @author rpaez
+ */
+public class Postulation {
+    private String idPostulation;
+    private String idStudent;
+    private String idBeca;
+    private String State;
+    
+    public Postulation(){
+        this.idPostulation = "";
+    }
+}
