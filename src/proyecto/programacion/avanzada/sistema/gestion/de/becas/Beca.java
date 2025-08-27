cf3ccb03e84026be59b1ea789502884eec479f59
Daniel Monsalve <daniel.monsalve.d@mail.pucv.cl>
2025-08-27 12:51:46 -0400

    Agregacion de la clase Beca

---
+++ src/proyecto/programacion/avanzada/sistema/gestion/de/becas/Beca.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyecto.programacion.avanzada.sistema.gestion.de.becas;
+
+/**
+ *
+ * @author Daniel Monsalve
+ */
+public class Beca {
+    private String codeBeca;
+    private String nomBeca;
+    private float porcAprob;
+    private String requisito;
+}
