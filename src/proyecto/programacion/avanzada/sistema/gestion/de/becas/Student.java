72c304e5a68a5b11b6fcbb7ed2b2d43cc5f98477
Ricardo25112005 <rpaezb251105@gmail.com>
2025-08-27 17:29:29 -0400

    Cambio de lenguaje del codigo a inglés en clase Student

--- src/proyecto/programacion/avanzada/sistema/gestion/de/becas/Student.java
+++ src/proyecto/programacion/avanzada/sistema/gestion/de/becas/Student.java
@@ -3,36 +3,49 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package proyecto.programacion.avanzada.sistema.gestion.de.becas;
-
+import java.util.LinkedList;
 /**
  *
  * @author rpaez
  */
 public class Student {
-    private String nombre;
+    private String name;
     private String rut;
-    private String direccion;
-    private String correo;
-    private int telefono;
-    private float tramoSocioEconomico;
-    private String carrera;
+    private String address;
+    private String mail;
+    private int phone;
+    private float socioEconomicSection;
+    private String carrer;
+    private LinkedList<Postulation> listPostulation;
     
     public Student(){
-        this.nombre = "";
+        this.name = "";
         this.rut = "";
-        this.direccion = "";
-        this.correo = "";
-        this.telefono = 0;
-        this.tramoSocioEconomico = 0.0f;
-        this.carrera = "";
+        this.address = "";
+        this.mail = "";
+        this.phone = 0;
+        this.socioEconomicSection = 0.0f;
+        this.carrer = "";
+        this.listPostulation = new LinkedList<>();
     }
     
-    public String getNombre(){
-        return nombre;
+    public Student(String name, String rut, String address, String mail, int phone, float socioEconomicSection, String carrer){
+        this.name = name;
+        this.rut = rut;
+        this.address = address;
+        this.mail = mail;
+        this.phone = phone;
+        this.socioEconomicSection = socioEconomicSection;
+        this.carrer = carrer;
+        this.listPostulation = new LinkedList<>();;
     }
     
-    public void setNombre(String nombre){
-        this.nombre = nombre;
+    public String getName(){
+        return name;
+    }
+    
+    public void setName(String name){
+        this.name = name;
     }
     
     public String getRut(){
@@ -43,43 +56,71 @@ public class Student {
         this.rut = rut;
     }
     
-    public String getDireccion(){
-        return direccion;
+    public String getAddress(){
+        return address;
     }
     
-    public void setDireccion(String direccion){
-        this.direccion = direccion;
+    public void setAddress(String address){
+        this.address = address;
     }
     
-    public String getCorreo(){
-        return correo;
+    public String getMail(){
+        return mail;
     }
     
-    public void setCorreo(String correo){
-        this.correo = correo;
+    public void setMail(String mail){
+        this.mail = mail;
     }
     
-    public int getTelefono(){
-        return telefono;
+    public int getPhone(){
+        return phone;
     }
     
-    public void setTelefono(int telefono){
-        this.telefono = telefono;
+    public void setPhone(int phone){
+        this.phone = phone;
     }
     
-    public float getTramoSocio(){
-        return tramoSocioEconomico;
+    public float getSocioEconomicSection(){
+        return socioEconomicSection;
     }
     
-    public void setTramoSocioEconomico(float tramoSocioEconomico){
-        this.tramoSocioEconomico = tramoSocioEconomico;
+    public void setSocioEconomicSection(float socioEconomicSection){
+        this.socioEconomicSection = socioEconomicSection;
     }
     
-    public String getCarrera(){
-        return carrera;
+    public String getCarrer(){
+        return carrer;
     }
     
-    public void setCarrera(String carrera){
-        this.carrera = carrera;
+    public void setCarrer(String carrer){
+        this.carrer = carrer;
+    }
+    
+    public void showData(){
+        System.out.println("=== Datos del Postulante ===");
+        System.out.println("Nombre: " + name);
+        System.out.println("RUT: " + rut);
+        System.out.println("Dirección: " + address);
+        System.out.println("Correo: " + mail);
+        System.out.println("Teléfono: " + phone);
+        System.out.println("Tramo Socioeconómico: " + socioEconomicSection);
+        System.out.println("Carrera: " + carrer);
+        System.out.println("\n=== Lista de Postulaciones ===");
+        if (listPostulation.isEmpty()){
+            System.out.println("No tiene postulaciones registradas.");
+        }
+        else{
+            for(Postulation p : listPostulation){
+                p.showPostulation();
+            }
+        }
+    }
+    
+    public void addPostulation(Postulation p){
+        listPostulation.add(p);
+    }
+    
+    public void removePostulation(Postulation p){
+        listPostulation.remove(p);
     }
 }
